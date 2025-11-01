b77a01b5e1b986c89c69495bde3c4431209ec54d
Rajini0510 <chevvururajini@gmail.com>
2025-11-01 18:58:07 +0530

    first commit

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,19 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
+# Set Chrome options
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless")  # run without UI (important inside Docker)
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# Create WebDriver instance
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
-time.sleep(2)
-driver.get("http://localhost:5000")
-
-assert "Hello World" in driver.page_source
-print("✅ Test Passed: 'Hello World' found on page")
+driver.get("http://127.0.0.1:5000")
+print("Page title is:", driver.title)
 
 driver.quit()
